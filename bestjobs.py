a296a14ac627f1b24705cce2d659c2dd022f1bca
AtaDemirkiran <atademirkiran@gmail.com>
2022-12-28 13:47:02 +0300

    first commit

---
+++ bestjobs.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+httpRequest=requests.get('https://www.glassdoor.com/List/Best-Jobs-in-America-2019-LST_KQ0,25.htm')
+
+html = httpRequest.content
+
+parsedHtml=BeautifulSoup(html,"html.parser")
+
+salaries=parsedHtml.find_all("div",{"data-test":"median-base-salary"})
+
+for salary in salaries:
+    print(salary.text)
